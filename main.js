e01119ab28d9db32ca2931b40c7dc14a620a7720
mackenziewritescode <mackenzie.charlton@gmail.com>
2020-09-09 21:53:50 -0400

    finished code

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
-  let defaultTime1 = 0.1;
-  let defaultTime2 = 1;
+  let defaultTime1 = 10;
+  let defaultTime2 = 5;
   let time1 = defaultTime1;
   let time2 = defaultTime2;
-  let minutes = time1;
-  let remainingSeconds = minutes * 60;
+  let remainingSeconds = time1 * 60;
   let interval;
   let running = false;
   let currentTimer1 = true;
+  let loop = true;
+  let audio = new Audio("audio/alarm.wav");
 
   $("#timer1-val").html(time1);
   $("#timer2-val").html(time2);
 
-  $("#display").html(formatTime(minutes * 60));
+  $("#display").html(formatTime(time1 * 60));
 
   function setTimer(timer, direction) {
     if (!running) {
       if (timer === "timer1") {
         if (direction === "up") {
-          if (minutes < 60) {
-            minutes += 1;
+          if (time1 < 60) {
+            time1 += 1;
           }
         } else if (direction === "down") {
-          if (minutes > 1) {
-            minutes -= 1;
+          if (time1 > 1) {
+            time1 -= 1;
           }
         }
       }
@@ -38,10 +39,17 @@ $(document).ready(function () {
           }
         }
       }
-      remainingSeconds = minutes * 60;
-      $("#timer1-val").html(minutes);
+      // if the current timer is timer1, setting timer1 will reset the running time on display and vice versa
+      if (currentTimer1 && timer === "timer1") {
+        remainingSeconds = time1 * 60;
+        $("#display").html(formatTime(time1 * 60));
+      } else if (!currentTimer1 && timer === "timer2") {
+        remainingSeconds = time2 * 60;
+        $("#display").html(formatTime(time2 * 60));
+      }
+      // setting the time on the timers
+      $("#timer1-val").html(time1);
       $("#timer2-val").html(time2);
-      $("#display").html(formatTime(minutes * 60));
     }
   }
 
@@ -52,27 +60,24 @@ $(document).ready(function () {
       console.log(remainingSeconds);
     } else {
       if (currentTimer1) {
-        currentTimer1 = !currentTimer1;
-        minutes = time2;
+        //if timer1 and loop on or off
         remainingSeconds = time2 * 60 + 1;
         updateTime();
-      } else {
-        currentTimer1 = !currentTimer1;
-        minutes = time1;
+      } else if (loop) {
+        // if timer2 and loop on
         remainingSeconds = time1 * 60 + 1;
         updateTime();
+      } else {
+        // if timer2 and loop off
+        clearInterval(interval);
+        interval = null;
+        running = false;
+        $("#play-img").attr("src", "/icons/play.svg");
+        remainingSeconds = time1 * 60;
       }
-    }
-  }
-
-  function startStop() {
-    if (running) {
-      clearInterval(interval);
-      interval = null;
-      running = false;
-    } else {
-      interval = setInterval(updateTime, 1000);
-      running = true;
+      // play audio
+      audio.play();
+      currentTimer1 = !currentTimer1;
     }
   }
 
@@ -87,6 +92,42 @@ $(document).ready(function () {
       seconds = "0".concat(seconds);
     }
     return minutes + ":" + seconds;
+  }
+
+  function startStop() {
+    if (running) {
+      clearInterval(interval);
+      interval = null;
+      running = false;
+      $("#play-img").attr("src", "/icons/play.svg");
+    } else {
+      interval = setInterval(updateTime, 1000);
+      running = true;
+      $("#play-img").attr("src", "/icons/pause.svg");
+    }
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  function setLoop() {
+    if (loop) {
+      $("#loop-img").attr("src", "/icons/loop-off.svg");
+      loop = false;
+    } else {
+      $("#loop-img").attr("src", "/icons/loop.svg");
+      loop = true;
+    }
+  }
+
+  function setReset() {
+    if (!running) {
+      time1 = defaultTime1;
+      time2 = defaultTime2;
+      remainingSeconds = time1 * 60;
+      $("#display").html(formatTime(time1 * 60));
+      $("#timer1-val").html(time1);
+      $("#timer2-val").html(time2);
+    }
   }
 
   $("#timer1-incr").click(function () {
@@ -104,15 +145,14 @@ $(document).ready(function () {
   });
 
   $("#reset-btn").click(function () {
-    time1 = defaultTime1;
-    time2 = defaultTime2;
-    minutes = time1;
-    remainingSeconds = minutes * 60;
-    $("#display").html(formatTime(minutes * 60));
-    $("#timer1-val").html(minutes);
+    setReset();
   });
 
   $("#start-btn").click(function () {
     startStop();
+  });
+
+  $("#loop-btn").click(function () {
+    setLoop();
   });
 });
